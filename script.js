66f7fc64faf2b96c266073a42cab3097e727bea9
bajarigergo <gergob10@gmail.com>
2024-02-29 12:16:14 +0100

    kachow

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
 window.addEventListener("load", function () {
-    elemekElerese1();
-    elemekElerese2();
-    elemekElerese3();
+  elemekElerese1();
+  elemekElerese2();
+  elemekElerese3();
+  elemekElerese4();
+  elemekFormázása();
+  esemenyKezeles();
+  esemenyKezeles2();
 });
 
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+  /**Ide jön az első feladat */
+  const feladat1_ELEM = document.querySelector("#f1");
+  console.log(feladat1_ELEM);
 }
-function elemekElerese1() {
-    /**Ide jön az 2.  feladat */
+function elemekElerese2() {
+  /**Ide jön az 2.  feladat */
+  feladat2_ELEM = document.querySelectorAll("#ide")[0];
+  feladat2_ELEM.innerHTML = "<p>Jó reggelt!</p>";
 }
-function elemekElerese1() {
-    /**Ide jön az 3. feladat */
+function elemekElerese3() {
+  /**Ide jön az 3. feladat */
+  feladat3_ELEM = document.querySelectorAll(".ide")[0];
+  feladat3_ELEM.innerHTML = "<p>Jó reggelt!</p>";
+}
+function elemekElerese4() {
+  /**Ide jön az 4. feladat */
+  const TOMB = [];
+  const feladat4_ELEM = document.querySelector(".lista");
+  for (let index = 0; index < 5; index++) {
+    let szam = Math.floor(Math.random() * (31 - 10) + 10);
+    TOMB[index] = szam;
+  }
+  console.log(TOMB);
+  feladat4_ELEM.innerHTML = "<ul>";
+  for (let index = 0; index < 5; index++) {
+    feladat4_ELEM.innerHTML += `<li>${TOMB[index]}</li>`;
+  }
+  feladat4_ELEM.innerHTML += "</ul>";
+}
+function elemekFormázása() {
+  const feladat4_ELEM = document.querySelector(".lista");
+  feladat4_ELEM.classList.add("formazott");
+}
+function esemenyKezeles() {
+  const feladat4_ELEM = document.querySelector(".lista");
+  const kattintasutan_ELEM = document.querySelector(".kattintasutan");
+  feladat4_ELEM.addEventListener("click", function () {
+    kattintasutan_ELEM.innerHTML = feladat4_ELEM.innerHTML;
+  });
+}
+function esemenyKezeles2() {
+  const feladat6_ELEM = document.querySelector(".feladat");
+  feladat6_ELEM.innerHTML = "<button>Nyomd meg</button>";
+  const gomb_ELEM = document.querySelector(".feladat button");
+  gomb_ELEM.addEventListener("click", function () {
+    feladat6_ELEM.innerHTML += `<div><h1>KACHOW</h1><img id="kep" src="./kep.jpg" alt="bajnok"></div>`;
+  });
 }
